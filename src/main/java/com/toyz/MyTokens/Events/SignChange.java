815fbbb282799779576841150ffd071ee7b15c76
LemADEC <lemadec.fr@gmail.com>
2015-01-18 00:27:58 +0100

    Code cleanup
    Fixed formating
    Updated to use explicit null checks
    Minor optimisation

--- src/main/java/com/toyz/MyTokens/Events/SignChange.java
+++ src/main/java/com/toyz/MyTokens/Events/SignChange.java
@@ -10,30 +10,25 @@ import org.bukkit.event.block.SignChangeEvent;
  * Created by Travis on 9/2/2014.
  */
 public class SignChange implements Listener {
-    @EventHandler(priority = EventPriority.HIGH)
-    public void on(SignChangeEvent e) {
-        if (e.isCancelled())
-            return;
+	@EventHandler(priority = EventPriority.HIGH)
+	public void on(SignChangeEvent e) {
+		if (e.isCancelled())
+			return;
 
-        //if(e.getPlayer().hasPermission("mytokens.sign.place")){
-            int max = e.getLines().length;
-            if(max >= 4){
-                 if(e.getLine(0).equalsIgnoreCase("[MyTokens]") || e.getLine(0).equalsIgnoreCase("[MYT]")){
-                     if(e.getPlayer().hasPermission("mytokens.sign.place")) {
-                         String last = e.getLine(3).replace(" ", "");
-                         e.setLine(3, last);
-                         e.getPlayer().sendMessage(MyTokens.getAPI().getMessageHelper().format(e.getPlayer(), "Placed Tokens shop at"));
-                     }else{
-                         e.setCancelled(true);
-                         return;
-                     }
-                 }
-            }else{
-                return;
-            }
-        /*}else{
-            //e.setCancelled(true);
-            return;
-        }*/
-    }
+		int max = e.getLines().length;
+		if (max >= 4) {
+			if (e.getLine(0).equalsIgnoreCase("[MyTokens]") || e.getLine(0).equalsIgnoreCase("[MYT]")) {
+				if (e.getPlayer().hasPermission("mytokens.sign.place")) {
+					String last = e.getLine(3).replace(" ", "");
+					e.setLine(3, last);
+					e.getPlayer().sendMessage(MyTokens.getAPI().getMessageHelper().format(e.getPlayer(), "Placed Tokens shop at"));
+				} else {
+					e.setCancelled(true);
+					return;
+				}
+			}
+		} else {
+			return;
+		}
+	}
 }
